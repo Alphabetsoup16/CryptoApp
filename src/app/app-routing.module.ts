1a50877ce5d3cf7cc6c4db2c074bd8d0445f8249
Alphabetsoup16 <81780117+Alphabetsoup16@users.noreply.github.com>
2022-07-01 14:48:02 -0400

    cool routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExchangesComponent } from './exchanges/exchanges.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'exchanges', component: ExchangesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
