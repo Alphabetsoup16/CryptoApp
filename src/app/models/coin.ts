f5b68f5505dc17a7087a82b40db417b53d7365c4
Alphabetsoup16 <81780117+Alphabetsoup16@users.noreply.github.com>
2022-07-05 14:15:16 -0400

    models

---
+++ src/app/models/coin.ts
@@ -0,0 +1,9 @@
+export interface Coin {
+    id: string,
+    name: string,
+    image: string,
+    current_price: number,
+    all_time_high: number,
+    market_cap: number,
+    circulating_supply: number,
+}
---
+++ src/app/models/exchange.ts
@@ -0,0 +1,10 @@
+export interface Exchange {
+    name: string,
+    country: string,
+    url: string,
+    image: string,
+    year_established: string,
+    trust_score: number,
+
+
+}
